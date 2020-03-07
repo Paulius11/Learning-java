60d41dadfd4477abfd9250c733b3a59ba3216c03
Paulius11 <paulius.gaizauskas@gmail.com>
2020-03-07 19:07:03 +0200

    Add validation

--- spring/users/src/main/java/lt/paulius/users/post/CreateUserCommand.java
+++ spring/users/src/main/java/lt/paulius/users/post/CreateUserCommand.java
@@ -1,10 +1,23 @@
 package lt.paulius.users.post;
 
-public class CreateUserCommand {
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
 
+import org.hibernate.validator.constraints.Length;
+
+public class CreateUserCommand {
+	@NotNull
+	@Length(min = 1, max = 30)
 	private String username;
+	@NotNull
+	@Length(min = 1, max = 100)
 	private String firstName;
+	@NotNull
+	@Length(min = 1, max = 100)
 	private String lastName;
+	@NotNull
+	@Length(min = 1, max = 200)
+	@Email
 	private String email;
 
 	public String getUsername() {
