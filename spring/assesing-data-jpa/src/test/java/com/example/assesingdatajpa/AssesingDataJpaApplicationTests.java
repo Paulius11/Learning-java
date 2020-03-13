65b27eebb82d39214907615216efc352c4e6b65d
Paulius11 <paulius.gaizauskas@gmail.com>
2020-03-13 13:38:11 +0200

    Add spring project with h2db

---
+++ spring/assesing-data-jpa/src/test/java/com/example/assesingdatajpa/AssesingDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.assesingdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssesingDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
